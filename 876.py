44c90732e6f628c2552a64ec27973dad299573d9
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-14 22:26:38 +0200

    cleaner solution to problem 876

--- 876.py
+++ 876.py
@@ -3,6 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+class Solution(object):
+    def middleNode(self, head):
+        # While slow moves one step forward, fast moves two steps forward.
+        # Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
