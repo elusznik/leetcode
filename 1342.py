9c2a0c5852e23f418f9755a26c8bc23f36739dfe
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-14 22:07:17 +0200

    bit counting solution to 1342

--- 1342.py
+++ 1342.py
@@ -1,10 +1,5 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        steps = 0
-        while num>0:
-            if num%2==0:
-                num=num//2
-            else:
-                num = num-1
-            steps+=1
-        return steps
+        if num == 0:
+            return 0
+        return num.bit_length() - 1 + num.bit_count()
