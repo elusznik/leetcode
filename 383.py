be4e924aed5bbc4f4e05a7460a8d0255842816d2
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-15 12:57:55 +0200

    faster 383 solution (collections.Counter)

--- 383.py
+++ 383.py
@@ -1,10 +1,8 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict = {}
-        for char in magazine:
-            dict[char] = dict.get(char, 0) + 1
-        for char in ransomNote:
-            if char not in dict or dict[char] == 0:
-                return False
-            dict[char] -= 1
-        return True
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
