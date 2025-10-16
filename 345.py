97455a27ab681a7fdaee91b3269cb4587a0cc6ad
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-16 16:07:36 +0200

    problem 345

--- 345.py
+++ 345.py
@@ -1,9 +1,16 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels=[list("aeiouyAEIOUY")]
-        s=list(s)
-        s_vowels=[]
-        for char in s:
-            if char in vowels:
-                s_vowels.append(char)
-                
+        vowels = set("aeiouAEIOU")
+        chars = list(s)
+        i, j = 0, len(chars) - 1
+        while i < j:
+            if chars[i] not in vowels:
+                i += 1
+                continue
+            if chars[j] not in vowels:
+                j -= 1
+                continue
+            chars[i], chars[j] = chars[j], chars[i]
+            i += 1
+            j -= 1
+        return "".join(chars)
