3b53390434f2e7f854b0a3cda5754f8f1e4353eb
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-16 16:12:50 +0200

    faster 345 solution

--- 345.py
+++ 345.py
@@ -4,12 +4,10 @@ class Solution:
         chars = list(s)
         i, j = 0, len(chars) - 1
         while i < j:
-            if chars[i] not in vowels:
+            while i < j and chars[i] not in vowels:
                 i += 1
-                continue
-            if chars[j] not in vowels:
+            while i < j and chars[j] not in vowels:
                 j -= 1
-                continue
             chars[i], chars[j] = chars[j], chars[i]
             i += 1
             j -= 1
