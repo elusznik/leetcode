20efdd75086c29555d55be09225722757abe1cfd
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-18 18:04:15 +0200

    1732 more memory efficient

--- 1732.py
+++ 1732.py
@@ -1,7 +1,8 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        alt = []
-        alt.append(0)
-        for i in range(1,len(gain)+1):
-            alt.append(alt[i-1]+gain[i-1])
-        return max(alt)
+        alt=0
+        current=0
+        for g in gain:
+            current+=g
+            alt=max(alt,current)
+        return alt
