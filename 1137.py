0fc8377f3095f036184bba99698cd597303207cf
Nikodem Eluszkiewicz <nikodem.eluszkiewicz.stud@pw.edu.pl>
2025-10-20 13:43:16 +0200

    more memory efficient 1137

--- 1137.py
+++ 1137.py
@@ -1,6 +1,5 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        t=[0,1,1]
-        for i in range(3,n+1):
-            t.append(t[i-3]+t[i-2]+t[i-1])
-        return t[n]
+        a, b, c = 1, 0, 0
+        for _ in range(n): a, b, c = b, c, a + b + c
+        return c
