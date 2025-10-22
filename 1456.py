bfc5f93ff368720a2adaa8d26a49cdd3448b1e0d
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-22 22:00:29 +0200

    1456 proper solution

--- 1456.py
+++ 1456.py
@@ -4,14 +4,17 @@ class Solution:
         start=0
         end=k
         substring=0
-        max_substring=0
+        for i in range(start,end):
+            if s[i] in vowels:
+                substring+=1
+        max_substring=substring             
         for i in range(len(s)-k+1):
-            for j in range(start,end):
-                if s[j] in vowels:
-                    substring+=1
             if substring>max_substring:
                 max_substring=substring
-            substring=0
+            if s[start] in vowels:
+                substring-=1
+            if end<len(s) and s[end] in vowels:
+                substring+=1
             start+=1
             end+=1
         return max_substring
