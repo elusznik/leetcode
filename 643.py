68eb7f6026739c1001c97907f89a994c9accaa23
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-22 23:02:56 +0200

    more elegant 643 solution

--- 643.py
+++ 643.py
@@ -1,11 +1,10 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         start=0
-        end=k
-        max_avg=sum(i for i in nums[:k])/k
-        current_sum=sum(nums[:k])
-        for end in range(k,len(nums)):
-            max_avg=max(max_avg, current_sum/k)
+        max_sum=current_sum=sum(nums[:k])
+        len_nums=len(nums)
+        for end in range(k,len_nums):
             current_sum=current_sum-nums[start]+nums[end]
             start+=1
-        return max(current_sum/k, max_avg)
+            max_sum=max(max_sum, current_sum)
+        return max_sum/k
