0decfa39c9fe7e22960d410dbe597e0f722cc007
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-22 22:42:01 +0200

    even faster 1456 solution

--- 1456.py
+++ 1456.py
@@ -8,15 +8,14 @@ class Solution:
             if s[i] in vowels:
                 substring+=1
         max_substring=substring             
-        for i in range(len(s)-k+1):
+        for end in range(k,len(s)):
             if substring>max_substring:
                 max_substring=substring
             if max_substring==k:
                 return k
             if s[start] in vowels:
                 substring-=1
-            if end<len(s) and s[end] in vowels:
+            if s[end] in vowels:
                 substring+=1
             start+=1
-            end+=1
-        return max_substring
+        return max(max_substring, substring)
