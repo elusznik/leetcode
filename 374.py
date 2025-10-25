3c76d155fbf7d66cc73366b87da05205a5ba9719
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-25 17:35:38 +0200

    slower, but built-in 374 solution

--- 374.py
+++ 374.py
@@ -7,15 +7,4 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        start=0
-        end=n
-        while start<=end:
-            mid=start+(end-start)//2
-            res=guess(mid)
-            if res==0:
-                return mid
-            elif res==1:
-                start=mid+1
-            else:
-                end=mid-1
-        return -1
+        return bisect_left(range(n), 0, key=lambda m: -guess(m))
