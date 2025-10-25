ddb34fb65bbcd69f25ab035f4e95ca0d23fa2e87
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-25 17:21:18 +0200

    cleaner 605 solution

--- 605.py
+++ 605.py
@@ -1,27 +1,11 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        placed=0
-        if len(flowerbed)==1:
-            if flowerbed[0]==0 and n<=1:
-                return True
-            elif flowerbed[0]==1 and n==0:
-                return True
-            else:
-                return False
-        if flowerbed[0]==0 and flowerbed[1]==0:
-            flowerbed[0]=1
-            placed+=1
-            if placed>=n:
-                return True
-        if flowerbed[-1]==0 and flowerbed[-2]==0:
-            flowerbed[-1]=1
-            placed+=1
-            if placed>=n:
-                return True
-        for i in range(1,len(flowerbed)-1):
-            if flowerbed[i]==0 and flowerbed[i-1]==0 and flowerbed[i+1]==0:
-                flowerbed[i]=1
-                placed+=1
-                if placed>=n:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
                     return True
-        return placed>=n
+        return False
