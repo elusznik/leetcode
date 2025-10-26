4301cb7d08640abb31a12f51e6cbd44daf2ce125
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-26 12:50:31 +0100

    cleaner 69 solution

--- 69.py
+++ 69.py
@@ -1,6 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        for i in range(x + 1):
-            if i * i > x:
-                return i - 1
-        return x
+        i = 1
+        while i * i <= x:
+            i += 1
+        return i - 1
