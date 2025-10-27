063b8707fb49357d1ccc942734c622b5afe2e4c8
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-27 20:08:54 +0100

    even faster problem 933 solution (popping front of queue)

--- 933.py
+++ 933.py
@@ -1,19 +1,13 @@
 class RecentCounter:
-
     def __init__(self):
-        self.requests=[]
+        self.requests = []
 
     def ping(self, t: int) -> int:
-        count=0
-        if self.requests and t-3000>=self.requests[-1]:
-            self.requests.clear()
         self.requests.append(t)
-        for req in self.requests:
-            if req>=t-3000:
-                count+=1
-            else:
-                del req
-        return count
+        window_start = t - 3000
+        while self.requests and self.requests[0] < window_start:
+            self.requests.pop(0)
+        return len(self.requests)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
