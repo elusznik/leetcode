4e742c5841979adba8305b2e2ea9e9f7fe2f1e12
Nikodem Eluszkiewicz <elusznik@gmail.com>
2025-10-27 20:19:36 +0100

    fastest 933 solution (deque)

--- 933.py
+++ 933.py
@@ -1,11 +1,14 @@
+from collections import deque
+
 class RecentCounter:
     def __init__(self):
-        self.requests = []
+        self.requests = deque()
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
-        while self.requests and self.requests[0] < t-3000:
-            self.requests.pop(0)
+        window_start = t - 3000
+        while self.requests and self.requests[0] < window_start:
+            self.requests.popleft()
         return len(self.requests)
 
 # Your RecentCounter object will be instantiated and called as such:
